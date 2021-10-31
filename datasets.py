415b65fe0c53ac57fc1d461aa2ac292ae7788d63
Ryo Yuki <iidx.ameto@gmail.com>
2021-10-31 18:26:55 +0900

    quickened the generation of hyperbolic geometric graphs

--- datasets.py
+++ datasets.py
@@ -55,6 +55,8 @@ def calc_dist_r(n_dim, sigma, R, div=INTEGRAL_DIV):
 
 
 def hyperbolic_geometric_graph(n_nodes, n_dim, R, sigma, T):
+    # TODO: プログラム前半部分も実行時間を短くする。
+    # 現状は次元の2乗オーダーの計算量
     # n_dimは2以上で
     x_polar = np.random.uniform(0, 1, (n_nodes, n_dim))
     # 逆関数法で点を双曲空間からサンプリング
@@ -68,16 +70,35 @@ def hyperbolic_geometric_graph(n_nodes, n_dim, R, sigma, T):
             idx = np.max(np.where(cum_dens <= x_polar[j, i])[0])
             x_polar[j, i] = val_array[idx]
     # 直交座標に変換(Euclid)
+    print('sampling ended')
+
     x_e = convert_euclid(x_polar)
 
+    print('convert euclid ended')
+
     adj_mat = np.zeros((n_nodes, n_nodes))
+    # 対角
+    sampling_mat = np.random.uniform(0, 1, adj_mat.shape)
+    sampling_mat = np.triu(
+        sampling_mat) + np.triu(sampling_mat).T - np.diag(sampling_mat.diagonal())
+
+    # できる限り行列演算を駆使して計算時間を短くする。
+    norm_x_e_2 = np.sum(x_e**2, axis=1).reshape((-1, 1))
+    denominator_mat = (1 - norm_x_e_2) * (1 - norm_x_e_2.T)
+    numerator_mat = norm_x_e_2 + norm_x_e_2.T
+    numerator_mat -= 2 * x_e.dot(x_e.T)
+    # arccoshのエラー対策
     for i in range(n_nodes):
-        for j in range(i + 1, n_nodes):
-            distance = h_dist(x_e[i], x_e[j])
-            prob = connection_prob(distance, R, T)
-            if np.random.uniform(0, 1) < prob:
-                adj_mat[i, j] = 1
-                adj_mat[j, i] = 1
+        numerator_mat[i, i] = 0
+    adj_mat = np.arccosh(1 + 2 * numerator_mat / denominator_mat)
+    adj_mat = connection_prob(adj_mat, R, T)
+    # 対角成分は必要ない
+    for i in range(n_nodes):
+        adj_mat[i, i] = 0
+    adj_mat = np.where(sampling_mat < adj_mat, 1, 0)
+
+    print("adj mat generated")
+
     return adj_mat
 
 
@@ -99,4 +120,5 @@ def convert_euclid(x_polar):
 
 if __name__ == '__main__':
     adj_mat = hyperbolic_geometric_graph(
-        n_nodes=1000, n_dim=10, R=10, sigma=1, T=2)
+        n_nodes=10000, n_dim=10, R=10, sigma=1, T=10)
+    print(adj_mat)
