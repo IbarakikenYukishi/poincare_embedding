550527787e0d14be9ea677c3bc2aebe49115e6a8
IbarakikenYukishi <iidx.ameto@gmail.com>
2021-11-19 16:39:21 +0900

    GPU embedding in experiment.py

--- experiment.py
+++ experiment.py
@@ -5,36 +5,42 @@ import torch
 import numpy as np
 import pandas as pd
 import gc
+import time
+from copy import deepcopy
 from torch.utils.data import DataLoader
 from datasets import hyperbolic_geometric_graph
-from embed import create_dataset, get_unobserved, Graph, SamplingGraph, RSGD, Poincare, calc_pc
+from embed import create_dataset, get_unobserved, Graph, SamplingGraph, RSGD, Poincare, calc_lik_pc_cpu
 
 if __name__ == '__main__':
     # データセット作成
     params_dataset = {
-        'n_nodes': 10000,
-        'n_dim': 5,
+        'n_nodes': 1000,
+        'n_dim': 32,
         'R': 10,
         'sigma': 1,
         'T': 2
     }
 
     # パラメータ
-    burn_epochs = 3
-    learning_rate = 10
-    burn_batch_size = 32
+    burn_epochs = 10
+    burn_batch_size = 1024
+    learning_rate = 10.0*burn_batch_size/32  # batchサイズに対応して学習率変更
     # SNML用
     snml_n_iter = 10
     snml_learning_rate = 0.1
     snml_n_max_data = 500
     # それ以外
-    loader_workers = 0
+    loader_workers = 16
+    print("loader_workers: ", loader_workers)
     shuffle = True
     sparse = True
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    n_graphs = 1
 
-    model_n_dims = [2, 3, 4, 5, 6, 7, 8]
 
-    for n_graph in range(1):  # データ5本で性能比較
+    model_n_dims = [2, 4, 8, 16, 32, 64, 128, 256]
+
+    for n_graph in range(n_graphs):  # データ5本で性能比較
         result = pd.DataFrame()
         print("n_graph:", n_graph)
         # 隣接行列
@@ -47,28 +53,36 @@ if __name__ == '__main__':
         )
         train, val = create_dataset(
             adj_mat=adj_mat,
-            n_max_positives=3,
-            n_max_negatives=30,
-            val_size=0.00001
+            n_max_positives=10,
+            n_max_negatives=100,
+            val_size=0.00002
         )
-
-        print(len(val))
 
         # 平均次数が少なくなるように手で調整する用
         print('average degree:', np.sum(adj_mat) / len(adj_mat))
 
         u_adj_mat = get_unobserved(adj_mat, train)
 
+        print('# of data in train:', len(train))
+        print('# of data in val:', len(val))
+
         print("start burn-in")
 
+
         for model_n_dim in model_n_dims:
+            train_ = deepcopy(train)
+            u_adj_mat_ = deepcopy(u_adj_mat)
+
             print("model_n_dim:", model_n_dim)
             # burn-inでの処理
+            start = time.time()
+
             dataloader = DataLoader(
-                Graph(train),
+                Graph(train_),
                 shuffle=shuffle,
                 batch_size=burn_batch_size,
                 num_workers=loader_workers,
+                pin_memory=True
             )
 
             # Rは決め打ちするとして、Tは後々平均次数とRから推定する必要がある。
@@ -81,6 +95,8 @@ if __name__ == '__main__':
                 sparse=True,
                 init_range=0.001
             )
+            model.to(device)  # GPUならGPUにおくっておく。
+
             # 最適化関数。
             rsgd = RSGD(model.parameters(), learning_rate=learning_rate,
                         R=params_dataset['R'])
@@ -92,8 +108,9 @@ if __name__ == '__main__':
                     rsgd.param_groups[0]["learning_rate"] /= 5
                 losses = []
                 for pairs, labels in dataloader:
+                    pairs = pairs.to(device)
+                    labels = labels.to(device)
                     rsgd.zero_grad()
-                    # print(model(pairs, labels))
                     loss = model(pairs, labels).mean()
                     loss.backward()
                     rsgd.step()
@@ -103,13 +120,21 @@ if __name__ == '__main__':
                 print("epoch:", epoch, ", loss:",
                       torch.Tensor(losses).mean().item())
 
-            # -2*log(p)の計算
-            basescore = 0
-            for pairs, labels in dataloader:
-                # print(model(pairs, labels))
-                loss = model(pairs, labels).sum().item()
-                basescore += 2 * loss
+            with torch.no_grad():
+                # -2*log(p)の計算
+                basescore = 0
+                for pairs, labels in dataloader:
+                    # print(model(pairs, labels))
+                    pairs = pairs.to(device)
+                    labels = labels.to(device)
+                    loss = model(pairs, labels).sum().item()
+                    basescore += 2 * loss
             print('basescore:', basescore)
+
+            elapsed_time = time.time() - start
+            print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+            model.to("cpu")
 
             # 以下ではmodelのみを流用する。
             # snmlの計算処理
@@ -127,69 +152,49 @@ if __name__ == '__main__':
             bic_history = []
 
             for data_idx, data in enumerate(dataloader_snml):
-                pair, label = data
+                # validationのデータ
+                val_pair, val_label = data
+                # print(val_pair)
+                # print(val_label)
 
                 # parametric complexityのサンプリングによる計算
                 sampling_data = SamplingGraph(
-                    adj_mat=u_adj_mat,
+                    adj_mat=u_adj_mat_,
                     n_max_data=snml_n_max_data,
                     positive_size=1 / 2  # 一様サンプリング以外は実装の修正が必要。
                 )
                 pairs, labels, n_possibles = sampling_data.get_all_data()
+                print("n_possibles:", n_possibles)
 
-                snml_pc = calc_pc(model, pairs, labels, n_possibles,
-                                  snml_n_iter, snml_learning_rate, params_dataset['R'], train)
+                lik, snml_pc = calc_lik_pc_cpu(model, val_pair, val_label, pairs, labels, n_possibles,
+                                  snml_n_iter, snml_learning_rate, params_dataset['R'], train_)
 
-                # valのデータでの尤度
-                # uとvに関わるデータを5個ずつサンプリングする。
-                u = pair[0, 0].item()
-                v = pair[0, 1].item()
-                u_indice = np.union1d(np.where(train[:, 0] == u)[
-                                      0], np.where(train[:, 1] == u)[0])
-                u_indice = np.random.permutation(u_indice)[0:5]
-                v_indice = np.union1d(np.where(train[:, 0] == v)[
-                                      0], np.where(train[:, 1] == v)[0])
-                v_indice = np.random.permutation(v_indice)[0:5]
-
-                pair_ = torch.cat((pair, torch.Tensor(
-                    train[u_indice, 0:2]), torch.Tensor(train[v_indice, 0:2])), dim=0).long()
-                label_ = torch.cat((label.reshape(-1, 1), torch.Tensor(train[u_indice, 2]).reshape(
-                    -1, 1), torch.Tensor(train[v_indice, 2]).reshape(-1, 1)), dim=0).long()
-
-                rsgd = RSGD(model.parameters(), learning_rate=snml_learning_rate,
-                            R=params_dataset['R'])
-                for _ in range(snml_n_iter):
-                    rsgd.zero_grad()
-                    loss = model(pair_, label_).mean()
-                    loss.backward()
-                    rsgd.step()
-
-                with torch.no_grad():
-                    lik = model(pair, label).item()
-                    basescore += 2 * lik
-                    snml_codelength += lik + snml_pc
-                    aic_history.append(
-                        basescore + 2 * (model_n_dim * params_dataset["n_nodes"]))
-                    bic_history.append(
-                        basescore + (model_n_dim * params_dataset["n_nodes"]) * np.log(len(train) + 1))
+                basescore += 2 * lik
+                snml_codelength += lik + snml_pc
+                aic_history.append(
+                    basescore + 2 * (model_n_dim * params_dataset["n_nodes"]))
+                bic_history.append(
+                    basescore + (model_n_dim * params_dataset["n_nodes"]) * np.log(len(train_) + 1))
 
                 print('snml_codelength ', data_idx, ':', snml_codelength)
                 # print('-log p:', loss.item())
                 # print('snml_pc:', snml_pc)
-                print(aic_history[-1])
-                print(bic_history[-1])
+                print('AIC ', data_idx, ':', aic_history[-1])
+                print('BIC ', data_idx, ':', bic_history[-1])
 
                 # valで使用したデータの削除
-                u_adj_mat[pair[0, 0], pair[0, 1]] = -1
-                u_adj_mat[pair[0, 1], pair[0, 0]] = -1
+                u_adj_mat_[val_pair[0, 0], val_pair[0, 1]] = -1
+                u_adj_mat_[val_pair[0, 1], val_pair[0, 0]] = -1
 
                 # データセットを更新する。
                 val_ = np.array(
-                    [pair[0, 0], pair[0, 1], label[0]]).reshape((1, -1))
-                train = np.concatenate([train, val_], axis=0)
+                    [val_pair[0, 0], val_pair[0, 1], val_label[0]]).reshape((1, -1))
+                train_ = np.concatenate([train_, val_], axis=0)
 
                 snml_codelength_history.append(snml_codelength)
 
+            del u_adj_mat_
+            del train_
             del dataloader
             del dataloader_snml
             gc.collect()
