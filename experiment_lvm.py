b00c07b2f20628bec445004d33ced5bae099635f
IbarakikenYukishi <iidx.ameto@gmail.com>
2022-02-11 05:41:20 +0900

    modified experiment lvm

--- experiment_lvm.py
+++ experiment_lvm.py
@@ -15,9 +15,13 @@ import torch.multiprocessing as multi
 from functools import partial
 
 
-def calc_metrics(device_idx, n_dim, n_nodes, n_graphs, n_devices):
+def calc_metrics(device_idx, n_dim, n_nodes, n_graphs, n_devices, model_n_dims):
 
     for n_graph in range(int(n_graphs * device_idx / n_devices), int(n_graphs * (device_idx + 1) / n_devices)):
+        pass_list = [0, 1, 2, 3, 5, 6, 7]
+        print(n_graph)
+        if n_graph in pass_list:
+            continue
 
         dataset = np.load('dataset/dim_' + str(n_dim) + '/graph_' + str(n_nodes) + '_' + str(n_graph) +
                           '.npy', allow_pickle='TRUE').item()  # object型なので、itemを付けないと辞書として使えない。
@@ -25,13 +29,13 @@ def calc_metrics(device_idx, n_dim, n_nodes, n_graphs, n_devices):
         params_dataset = dataset["params_adj_mat"]
 
         # パラメータ
-        burn_epochs = 200
+        burn_epochs = 300
         burn_batch_size = min(int(params_dataset["n_nodes"] * 0.2), 100)
         n_max_positives = int(params_dataset["n_nodes"] * 0.02)
         n_max_negatives = n_max_positives * 10
         learning_rate = 10.0 * \
             (burn_batch_size * (n_max_positives + n_max_negatives)) / \
-            32  # batchサイズに対応して学習率変更
+            32 / 100  # batchサイズに対応して学習率変更
         sigma_max = 1.0
         sigma_min = 0.001
         beta_min = 0.1
@@ -52,15 +56,16 @@ def calc_metrics(device_idx, n_dim, n_nodes, n_graphs, n_devices):
         result = pd.DataFrame()
         basescore_y_and_z_list = []
         basescore_y_given_z_list = []
+        basescore_z_list = []
         DNML_codelength_list = []
+        pc_first_list = []
+        pc_second_list = []
         AIC_naive_list = []
         BIC_naive_list = []
         CV_score_list = []
 
-        model_n_dims = [2, 3, 4, 5, 6, 7, 8]
-
         for model_n_dim in model_n_dims:
-            basescore_y_and_z, basescore_y_given_z, DNML_codelength, AIC_naive, BIC_naive = DNML_HGG(
+            basescore_y_and_z, basescore_y_given_z, basescore_z, DNML_codelength, pc_first, pc_second, AIC_naive, BIC_naive = DNML_HGG(
                 adj_mat=adj_mat,
                 params_dataset=params_dataset,
                 model_n_dim=model_n_dim,
@@ -80,7 +85,10 @@ def calc_metrics(device_idx, n_dim, n_nodes, n_graphs, n_devices):
             )
             basescore_y_and_z_list.append(basescore_y_and_z)
             basescore_y_given_z_list.append(basescore_y_given_z)
+            basescore_z_list.append(basescore_z)
             DNML_codelength_list.append(DNML_codelength)
+            pc_first_list.append(pc_first)
+            pc_second_list.append(pc_second)
             AIC_naive_list.append(AIC_naive)
             BIC_naive_list.append(BIC_naive)
 
@@ -107,11 +115,14 @@ def calc_metrics(device_idx, n_dim, n_nodes, n_graphs, n_devices):
 
         result["model_n_dims"] = model_n_dims
         result["DNML_codelength"] = DNML_codelength_list
+        result["pc_first"] = pc_first_list
+        result["pc_second"] = pc_second_list
         result["AIC_naive"] = AIC_naive_list
         result["BIC_naive"] = BIC_naive_list
         # result["CV_score"] = CV_score_list
         result["basescore_y_and_z"] = basescore_y_and_z_list
         result["basescore_y_given_z"] = basescore_y_given_z_list
+        result["basescore_z"] = basescore_z_list
 
         result.to_csv("results/result_" + str(n_dim) + "_" + str(n_nodes) +
                       "_" + str(n_graph) + ".csv", index=False)
@@ -129,9 +140,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
-    n_nodes_list = [100, 200, 400, 800, 1600]
+    # HGG-5
+    # n_nodes_list = [400, 800, 1600, 3200, 6400]
+    # model_n_dims = [2, 3, 4, 5, 6, 7, 8]
+
+    # HGG-15
+    # n_nodes_list = [400, 800, 1600, 3200, 6400]
+    # model_n_dims = [6, 9, 12, 15, 18, 21, 24]
+
+    # HGG-4
+    # n_nodes_list = [400, 800, 1600, 3200, 6400]
+    n_nodes_list = [6400]
+    model_n_dims = [2, 4, 8, 16, 32, 64]
 
     for n_nodes in n_nodes_list:
 
         calc_metrics(device_idx=int(args.device), n_dim=int(args.n_dim),
-                     n_nodes=n_nodes, n_graphs=10, n_devices=4)
+                     n_nodes=n_nodes, n_graphs=10, n_devices=4, model_n_dims=model_n_dims)
