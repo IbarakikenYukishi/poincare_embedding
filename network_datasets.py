96f162570479595dd93efc8369dba5b764202f70
IbarakikenYukishi <iidx.ameto@gmail.com>
2023-06-02 23:32:03 +0900

    added network_datasets

--- network_datasets.py
+++ network_datasets.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from scipy import integrate, stats
-# from embed import create_dataset
 from copy import deepcopy
 import pandas as pd
 from sklearn.metrics import label_ranking_average_precision_score, average_precision_score
@@ -14,7 +13,6 @@ from multiprocessing import Pool
 from functools import partial
 import stellargraph as sg
 from utils.utils_dataset import create_test_for_link_prediction
-# import networkx as nx
 
 import networkx as nx
 import pandas as pd
@@ -23,14 +21,6 @@ import tarfile
 import sys
 import pickle as pkl
 from scipy.sparse import coo_matrix
-
-# if not os.path.exists("dataset/cora"):
-#     wget.download(
-#         "https://linqs-data.soe.ucsc.edu/public/lbc/cora.tgz", out="dataset")
-#     tar_path = 'dataset/cora.tgz'
-
-#     with tarfile.open(tar_path, 'r:gz') as tar:
-#         tar.extractall(path="dataset")
 
 # # airport dataset
 # os.makedirs("dataset/airport", exist_ok=True)
@@ -107,12 +97,29 @@ def load_data_lp(dataset, data_path):
     data = adj
     return data
 
+
 def data_generation(dataset_name):
-    adj_mat = load_data_lp(dataset_name, "dataset/"+dataset_name)
+    print(dataset_name)
+    adj_mat = load_data_lp(dataset_name, "dataset/" + dataset_name)
     adj_mat = adj_mat.toarray().astype(np.int)
 
     n_nodes = adj_mat.shape[0]
+
+    # 無向グラフなので有向グラフに変換する
+    adj_u = np.triu(adj_mat) + np.triu(adj_mat).T
+    adj_l = np.tril(adj_mat) + np.tril(adj_mat).T
+
+    for i in range(n_nodes):
+        adj_u[i, i] = 0
+        adj_l[i, i] = 0
+
+    adj_mat = np.maximum(adj_u, adj_l)
+    adj_mat = np.minimum(adj_mat, 1)
+    adj_mat = np.maximum(adj_mat, 0)
+
     print("n_nodes:", n_nodes)
+    print("n_edges:", np.sum(adj_mat) / 2)
+    print(adj_mat)
 
     params_dataset = {
         "n_nodes": n_nodes
@@ -135,26 +142,3 @@ if __name__ == "__main__":
     data_generation("airport")
     data_generation("cora")
     data_generation("pubmed")
-
-    # print(load_data_lp("airport", "dataset/airport"))
-    # print(load_data_lp("pubmed", "dataset/pubmed"))
-    # print(load_data_lp("cora", "dataset/cora"))
-
-    # # Coraデータの読み込み
-    # cora_content = pd.read_csv('dataset/cora/cora.content', sep='\t', header=None)
-    # cora_cites = pd.read_csv('dataset/cora/cora.cites', sep='\t', header=None)
-
-    # # ノード属性を表示
-    # print("Node attributes:")
-    # print(cora_content.head())
-
-    # # エッジ（引用関係）を表示
-    # print("\nEdge (citation) relationships:")
-    # print(cora_cites.head())
-
-    # # NetworkXグラフの作成
-    # G = nx.from_pandas_edgelist(cora_cites, source=0, target=1)
-
-    # # グラフの情報を表示
-    # print("\nGraph information:")
-    # print(nx.info(G))
