36df07eee38434438190bfc8933bf536cfd70089
IbarakikenYukishi <iidx.ameto@gmail.com>
2023-06-02 23:56:08 +0900

    modified new shellscript

--- experiment_link.sh
+++ experiment_link.sh
@@ -1,32 +1,36 @@
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 2 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 3 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 4 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 5 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 6 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 7 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 8 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 0 9 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 2 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 3 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 4 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 5 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 6 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 7 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 8 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 1 9 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 2 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 3 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 4 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 5 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 6 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 7 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 8 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 2 9 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 2 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 3 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 4 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 5 3'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 6 0'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 7 1'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 8 2'
-screen -dm bash -c 'conda activate embed; python experiment_realworld_lorentz.py 3 9 3'
+#!/bin/bash
+echo "dataset: $1"
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 2 0"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 3 1"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 4 2"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 5 3"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 6 0"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 7 1"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 8 2"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 9 3"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 10 0"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 11 1"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 12 2"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 13 3"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 14 0"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 15 1"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 16 2"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 32 3"
+sleep 2
+screen -dm bash -c "conda activate embed; python experiment_realworld_lorentz.py ${1} 64 3"
+sleep 2
